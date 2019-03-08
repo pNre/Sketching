754cb955ebb99b52806fa278798b1d6c02c42541
Pierluigi <mail@pierluigi.me>
2019-03-08 19:57:48 +0100

    Add HyperLogLog documentation

--- Sources/Sketching/HyperLogLog.swift
+++ Sources/Sketching/HyperLogLog.swift
@@ -9,81 +9,91 @@ import Foundation
 
 public struct HyperLogLog<Hasher: Hashing> {
 
-    private let p: Int
+    /// Precision.
+    private let b: Int
+
+    /// Number of registers.
     private let m: Int
+
+    /// Internal state.
     private var registers: [UInt8]
-    private var maxRank: Int {
-        return 32 - p
+
+    /// Creates an empty `HyperLogLog`.
+    ///
+    /// - Precondition: `4 ≤ b ≤ 16`
+    /// - Parameter b: Determines the number of registers to use, `2^b`.
+    public init(precision b: Int = 8) {
+        precondition(b >= 4 && b <= 16)
+        self.b = b
+        self.m = 1 << b
+        self.registers = Array(repeating: 0, count: m)
     }
 
-    private var alpha: Double {
-        switch p {
-        case 4:
-            return 0.673
-        case 5:
-            return 0.697
-        case 6:
-            return 0.709
-        default:
-            return 0.7213 / (1.0 + 1.079 / Double(1 << p))
-        }
+    /// Inserts the given element in the `HyperLogLog`.
+    ///
+    /// - Parameter newMember: An element to insert into the `HyperLogLog`.
+    public mutating func insert<S: Sequence>(_ newMember: S) where S.Element == UInt8 {
+        let x = Hasher.hash(newMember, upperBound: UInt32.max).makeIterator().next()!
+        let index = 1 + Int(x & UInt32(m - 1))
+        let w = x >> b
+        let pw = w == 0 ? 0 : UInt8(w.leadingZeroBitCount + 1)
+        registers[index] = max(registers[index], pw)
     }
 
-    public init(precision: Int = 8) {
-        precondition(precision >= 4 && precision <= 16)
-        p = precision
-        m = 1 << precision
-        registers = Array(repeating: 0, count: m)
-    }
-
-    private init(state: [UInt8]) {
-        m = state.count
-        p = m.bitWidth - m.leadingZeroBitCount - 1
-        registers = state
-    }
-
-    public mutating func insert<C: Collection>(_ v: C) where C.Element == UInt8 {
-        let hashedValue = Hasher.hash(v, upperBound: UInt32.max).makeIterator().next()!
-        let index = hashedValue & UInt32(m - 1)
-        let bits = hashedValue >> p
-        registers[Int(index)] = max(registers[Int(index)], UInt8(rank(of: bits)))
-    }
-
+    /// Approximates the number of items contained in the set.
+    ///
+    /// - Returns: Estimate of the number of items in the set.
     public func cardinality() -> Double {
+        let alpha: Double = {
+            switch m {
+            case 4:
+                return 0.673
+            case 5:
+                return 0.697
+            case 6:
+                return 0.709
+            default:
+                return 0.7213 / (1.0 + 1.079 / Double(m))
+            }
+        }()
+
         let r = registers.reduce(0, { (sum, x) in sum + pow(2, -Double(x)) })
         let E = alpha * Double(m * m) / r
 
-        if E <= (5.0 / 2.0) * Double(m) {
+        if E < (5.0 / 2.0) * Double(m) {
             let V = registers.filter { $0 == 0 }.count
-            if V > 0 {
-                return Double(m) * log(Double(m) / Double(V))
-            } else {
+            guard V > 0 else {
                 return E
             }
-        } else if E <= (1.0 / 30.0) * Double(1 << 32) {
+
+            return Double(m) * log(Double(m) / Double(V))
+        } else if E <= Double(1 << 32) / 30.0 {
             return E
         }
 
         return -Double(1 << 32) * log(1.0 - E / Double(1 << 32))
     }
 
+    /// Returns a new `HyperLogLog` resulting from the union of the receiver and `other`.
+    ///
+    /// - Precondition: Both `HyperLogLog` must have the same number of registers.
+    /// - Parameter other: An `HyperLogLog` with the same number of registers as the receiver.
+    /// - Returns: `HyperLogLog` representing the union of both the receiver and `other`.
     public func union(_ other: HyperLogLog) -> HyperLogLog {
         var union = self
         union.formUnion(other)
         return union
     }
 
+    /// Adds the elements of the given set to the receiver.
+    ///
+    /// - Precondition: Both `HyperLogLog` must have the same number of registers.
+    /// - Parameter other: An `HyperLogLog` of the same type as the receiver.
     public mutating func formUnion(_ other: HyperLogLog) {
         precondition(other.m == m, "To form an union both HyperLogLog must have the same number of registers")
         for i in 0..<registers.count {
             registers[i] = max(registers[i], other.registers[i])
         }
-    }
-
-    private func rank(of value: UInt32) -> Int {
-        let r = maxRank - (value.bitWidth - value.leadingZeroBitCount) + 1
-        precondition(r > 0)
-        return r
     }
 
 }
