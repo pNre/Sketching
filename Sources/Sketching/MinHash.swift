1db77cd91c0dd16a6c151a47e3d027a8715f43e8
Pierluigi <mail@pierluigi.me>
2019-03-08 17:06:00 +0100

    Add documentation to MinHash

--- Sources/Sketching/MinHash.swift
+++ Sources/Sketching/MinHash.swift
@@ -9,29 +9,45 @@ import Foundation
 
 public struct MinHash<Hasher: Hashing> {
 
+    /// Number of hash functions used by the receiver.
     public var hashCount: Int {
         return hashValues.count
     }
 
     private var hashValues: [UInt32]
 
+    /// Creates an empty `MinHash`.
+    ///
+    /// - Parameter hashCount: Number of hash functions to use.
     public init(hashCount: Int) {
         self.hashValues = Array(repeating: .max, count: hashCount)
     }
 
-    public mutating func insert<C: Collection>(_ v: C) where C.Element == UInt8 {
-        let hashes = Hasher.hash(v, upperBound: UInt32.max)
-        for (index, hash) in hashes.prefix(hashValues.count).enumerated() where hash < hashValues[index] {
-            hashValues[index] = hash
+    /// Inserts the given element in the `MinHash` if it is not already present.
+    ///
+    /// - Parameter newMember: An element to insert into the `MinHash`.
+    public mutating func insert<S: Sequence>(_ newMember: S) where S.Element == UInt8 {
+        let hashes = Hasher.hash(newMember, upperBound: UInt32.max).makeIterator()
+        for index in hashValues.indices {
+            if let hash = hashes.next(), hash < hashValues[index] {
+                hashValues[index] = hash
+            }
         }
     }
 
+    /// Returns a new `MinHash` resulting from the merge of the receiver and `other`.
+    ///
+    /// - Parameter other: A `MinHash` with the same `hashCount` as the receiver.
+    /// - Returns: `MinHash` with the elements of both the receiver and `other`.
     public func union(_ other: MinHash) -> MinHash {
         var union = self
         union.formUnion(other)
         return union
     }
 
+    /// Merges `other` into the receiver.
+    ///
+    /// - Parameter other: A `MinHash` with the same `hashCount` as the receiver.
     public mutating func formUnion(_ other: MinHash) {
         precondition(hashCount == other.hashCount, "Both MinHash must be initialized with the same hashCount")
         for i in 0..<hashCount {
@@ -39,11 +55,15 @@ public struct MinHash<Hasher: Hashing> {
         }
     }
 
+    /// Estimates the Jaccard similarity coefficient between the `MinHash` and the receiver.
+    ///
+    /// - Parameter other: A `MinHash`.
+    /// - Returns: Jaccard similarity coefficient.
     public func jaccard(_ other: MinHash) -> Double {
         precondition(hashCount == other.hashCount, "Both MinHash must be initialized with the same hashCount")
 
         var intersection = 0
-        for i in 0..<hashCount where hashValues[i] == other.hashValues[i] {
+        for (a, b) in zip(hashValues, other.hashValues) where a == b {
             intersection += 1
         }
 
