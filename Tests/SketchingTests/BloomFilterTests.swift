056815cd7612f4ddff37f08335bc3bb95c28a946
Pierluigi <mail@pierluigi.me>
2019-03-08 20:03:27 +0100

    Add BloomFilter contains test

--- Tests/SketchingTests/BloomFilterTests.swift
+++ Tests/SketchingTests/BloomFilterTests.swift
@@ -20,4 +20,26 @@ class BloomFilterTests: XCTestCase {
         XCTAssertEqual(f.cardinality(), 0)
     }
 
+    func testContains() {
+        let base = Set((0..<64).map { _ in randomBytes(16) })
+        let test = Set((0..<64).map { _ in randomBytes(16) }).union(base)
+
+        var f = BloomFilter<FNV1AHashing>(expectedCardinality: base.count, probabilityOfFalsePositives: 0.001)
+        base.forEach {
+            f.insert($0)
+        }
+
+        for x in test where !f.contains(x) {
+            XCTAssertFalse(base.contains(x))
+        }
+    }
+
+}
+
+extension BloomFilterTests {
+
+    private func randomBytes(_ length: Int) -> [UInt8] {
+        return (0..<length).map { _ in UInt8.random(in: 0..<UInt8.max) }
+    }
+
 }
